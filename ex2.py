c866ae0486bf7a55128d18ab225f3cc1356ce5a2
LotanLevy <lotan.levy@mail.huji.ac.il>
2019-12-22 08:52:17 +0200

    remove iumage printing

--- ex2.py
+++ ex2.py
@@ -97,7 +97,6 @@ def train_main(max_iterations, image_trainer, trained_image, print_freq, max_pre
             loss_plotter.add("loss", i+1, image_trainer.train_loss.result())
             pred_plotter.add("prediction", i+1, image_trainer.last_pred.result())
     print("Training is stop after {} iterations".format(i))
-    print(trained_image)
 
     loss_plotter.plot()
     pred_plotter.plot()
