33702859a37c3e7384249589ecff2c5ee49d904a
LotanLevy <lotan.levy@mail.huji.ac.il>
2019-12-22 08:49:01 +0200

    change lr

--- ex2.py
+++ ex2.py
@@ -74,7 +74,7 @@ def get_optimizer(optimizer_type):
     :return: optimizer object
     """
     if optimizer_type == "adam":
-        return tf.keras.optimizers.Adam()
+        return tf.keras.optimizers.Adam(learning_rate=1e-2)
     return None
 
 
