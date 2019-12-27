1199cd4ecf27313645a86fb337d7a7bf2d08c31a
naomideutsch <naomi.deutsch@mail.huji.ac.il>
2019-12-27 12:09:14 +0200

    trying Raanan eq.

--- train_test.py
+++ train_test.py
@@ -35,7 +35,7 @@ class ImageTrainer:
         loss = -prediction
         if self.loss_name == "Fourier":
             fft_image = tf.signal.fft(tf.cast(image, tf.complex64))
-            w = tf.cast(tf.linalg.normalize(fft_image, axis=[-2,-1]), tf.float32)
+            w = tf.cast(tf.linalg.normalize(fft_image, axis=[-3,-2,-1]), tf.float32)
 
             print("-----------------")
             tf.print(w)
